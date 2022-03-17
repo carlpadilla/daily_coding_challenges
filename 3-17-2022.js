f23021a3e236d33ec2ca70f479d48f2e5692ef9e
carlpadilla <carlpadilla@protonmail.com>
2022-03-17 17:07:39 -0400

    added 3-17-2022

---
+++ 3-17-2022.js
@@ -0,0 +1,11 @@
+// 8kyu - Hello, Name or World!
+function hello(name) {
+  if (name === '' || name === undefined) {
+    return `Hello, World!`;
+  } else if (name) {
+    let fName = name.toLowerCase();
+    let firstLetter = fName[0].toUpperCase();
+    let checkName = firstLetter + fName.slice(1);
+    return `Hello, ${checkName}!`;
+  }
+}
