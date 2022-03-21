f412f91e1d0bae5b83d1b309aad7eb6893c95a72
carlpadilla <carlpadilla@protonmail.com>
2022-03-21 10:03:16 -0400

    adding 3-21-22

--- 3-21-2022.js
+++ 3-21-2022.js
@@ -7,3 +7,15 @@ const oddCount = (n) => Math.floor(n / 2);
 
 oddCount(7); //=> 3, i.e [1, 3, 5]
 oddCount(15); //=> 7, i.e [1, 3, 5, 7, 9, 11, 13]
+
+//--------------------------------------------------------------------------------
+
+//8kyu - Sentence Smash
+//Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+
+// function smash(words) {
+//   return words.join(' ');
+// }
+const smash = (words) => words.join` `;
+
+smash(['hello', 'world', 'this', 'is', 'great']); // =>  'hello world this is great'
