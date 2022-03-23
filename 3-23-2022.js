14ee015fdff2b5ebdb50ba1d54661672837b7787
carlpadilla <carlpadilla@protonmail.com>
2022-03-23 13:18:05 -0400

    added 3-23-2022

---
+++ 3-23-2022.js
@@ -0,0 +1,27 @@
+//7kyu - Case Swap
+//Given a string, swap the case for each of the letters.
+
+// e.g. CodEwArs --> cODeWaRS
+
+function swap(str) {
+  // new Array to push case swapped letters into
+  const newStr = [];
+  //split the word into separate letters array
+  let split = str.split('');
+
+  //loop through letters
+  for (let i = 0; i < split.length; i++) {
+    //Check if letter is capitalized
+    if (split[i].toUpperCase() === split[i]) {
+      //push cap letter lowercased into new array
+      newStr.push(split[i].toLowerCase());
+    } else {
+      //push lower case letter capitalized into new array
+      newStr.push(split[i].toUpperCase());
+    }
+  }
+  // join letters back in word to return
+
+  return newStr.join('');
+}
+swap('CodEwArs');
