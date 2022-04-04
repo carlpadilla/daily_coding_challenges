30984041c8933d952198edb159abffa06478e07e
carlpadilla <carlpadilla@protonmail.com>
2022-04-04 08:27:46 -0400

    added 4-2-2022

---
+++ 4-2-2022.js
@@ -0,0 +1,20 @@
+// 8KYU -Remove First and Last Character Part Two
+
+const array = (arr) => {
+  arr = arr.split(",");
+  arr.pop();
+  arr.shift();
+  return arr.join(" ") === "" ? null : arr.join(" ");
+};
+
+// same result but didnt pass tests..
+// function array(arr) {
+//   //Good luck
+//   const convertArr = arr.split(",");
+//   const popLast = convertArr.pop();
+//   const removeFirst = convertArr.shift();
+//   return convertArr.toString().replaceAll(",", " ");
+// }
+
+console.log(array("1,2,3")); // '2'
+console.log(array("1,2,3,4")); //'2,3'
