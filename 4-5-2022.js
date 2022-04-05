0b5d977c7ea5a95e8a8fb4b63f67e669f8a48698
carlpadilla <carlpadilla@protonmail.com>
2022-04-05 07:30:49 -0400

    added 4-5-2022

---
+++ 4-5-2022.js
@@ -0,0 +1,13 @@
+//8 kyu - Beginner - Lost Without a Map
+
+/*
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6]
+
+
+*/
+
+function maps(x) {}
