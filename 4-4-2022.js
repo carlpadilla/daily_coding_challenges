2d7d7d53ff5964fde4f8439b77b6cbad63dc8422
Carl Padilla <carlpadilla@protonmail.com>
2022-04-08 09:06:07 -0400

    added 4-4-2022

--- 4-4-2022.js
+++ 4-4-2022.js
@@ -11,4 +11,16 @@ rps('paper','paper') // Draw!
 
 */
 
-const rps = (p1, p2) => {};
+const rps = (p1, p2) => {
+  if (
+    (p1 === "rock" && p2 === "scissors") ||
+    (p1 === "paper" && p2 === "rock") ||
+    (p1 === "scissors" && p2 === "paper")
+  ) {
+    return "Player 1 won!";
+  } else if (p1 === p2) {
+    return "Draw!";
+  } else {
+    return "Player 2 won!";
+  }
+};
