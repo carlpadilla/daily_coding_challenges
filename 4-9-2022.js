42138f555712b3fd60e86ebc50957767c80a52a5
Carl Padilla <carlpadilla@protonmail.com>
2022-04-11 21:49:05 -0400

    added 4-9-2022

---
+++ 4-9-2022.js
@@ -0,0 +1,26 @@
+// 8kyu - Check same case
+
+/*
+
+Write a function that will check if two given characters are the same case.
+
+If any of characters is not a letter, return -1
+If both characters are the same case, return 1
+If both characters are letters and not the same case, return 0
+Examples
+'a' and 'g' returns 1
+
+'A' and 'C' returns 1
+
+'b' and 'G' returns 0
+
+'B' and 'g' returns 0
+
+'0' and '?' returns -1
+
+
+*/
+
+function sameCase(a, b) {
+  return 0;
+}
