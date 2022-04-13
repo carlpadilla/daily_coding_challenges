4b8099fbabe2f358a20297bbfcd7355279d4bfec
Carl Padilla <carlpadilla@protonmail.com>
2022-04-13 11:47:37 -0400

    added 4-10-2022

--- 4-10-2022.js
+++ 4-10-2022.js
@@ -12,10 +12,12 @@ Note:
 Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
 
+
 */
 
-function betterThanAverage(classPoints, yourPoints) {
-  // Your code here
-}
+const betterThanAverage = (classPoints, yourPoints) =>
+  classPoints.reduce((a, b) => a + b, 0) / classPoints.length < yourPoints
+    ? true
+    : false;
 
 betterThanAverage([2, 3], 5); //true
