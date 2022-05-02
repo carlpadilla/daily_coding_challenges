7b7cc6a50b46791048039395317b0cc483a9db3c
carlpadilla <carlpadilla@protonmail.com>
2022-05-02 11:18:38 -0400

    added 5-1-2022

---
+++ 5-1-2022.js
@@ -0,0 +1,19 @@
+//7 KYU - String Merge!
+
+/*
+Given two words and a letter, return a single word that's a combination of both words, merged at the point where the given letter first appears in each word. The returned word should have the beginning of the first word and the ending of the second, with the dividing letter in the middle. You can assume both words will contain the dividing letter.
+Examples
+
+("hello", "world", "l")       ==>  "held"
+("coding", "anywhere", "n")   ==>  "codinywhere"
+("jason", "samson", "s")      ==>  "jasamson"
+("wonderful", "people", "e")  ==>  "wondeople"
+
+
+*/
+
+function stringMerge(string1, string2, letter) {
+  const indx1 = string1.indexOf(letter)
+  const indx2 = string2.indexOf(letter)
+  return string1.slice(0, indx1) + string2.slice(indx2)
+}
